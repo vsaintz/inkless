ce33f60ece6d84b276a3fa7eb63ab96726429e35
vsaintz <vsaintz.dev@gmail.com>
2026-02-01 19:17:33 +0530

    clean up components and initialize css variable theming
    migrated the auth-banner to a standalone component to flatten the directory structure and reduce boilerplate. 
    also replaced previously hardcoded color values with a system of css variables.

--- src/app/auth/components/auth-banner/auth-banner.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core"
-
-@Component({
-  selector: 'app-auth-banner',
-  imports: [],
-  templateUrl: './auth-banner.html',
-  styles: ``,
-})
-export class AuthBanner {}
